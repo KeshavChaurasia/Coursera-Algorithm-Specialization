86be70c1f38182608d697ef47725ee982f2f51df
keshavchaurasia <keshav.chaurasia1995@gmail.com>
2019-08-19 14:28:13 +0545

    car fueling problem done

--- algorithmic-toolbox/week3/3-3-car_fueling.cpp
+++ algorithmic-toolbox/week3/3-3-car_fueling.cpp
@@ -9,7 +9,42 @@ using std::max;
 int compute_min_refills(int dist, int tank, vector<int> & stops) {
     // write your code here
     int no_of_refill = -1;
-    int no_of_stop = stops.size();
+    int total_stop = stops.size();
+    int current_fuel = 0;
+    int start = 0;
+    int i = 0;
+    int end = stops[i];
+    int total_distance_covered = 0;
+    while(total_distance_covered < dist){
+        // cout << "start: " << start << " end: " << end << "\n";
+        // cout << "fuel before: " << current_fuel  << "\n";
+        int distance_to_cover = end - start ;
+        // cout << "distance to cover: " << distance_to_cover  << "\n";
+        if (distance_to_cover > current_fuel){
+            current_fuel = tank;
+            no_of_refill ++ ;
+            // cout << "no_of_refill: " << no_of_refill  << "\n";
+            // cout << "fuel after: " << current_fuel  << "\n";
+        }
+        if (distance_to_cover > current_fuel){
+            return -1;
+        }
+        else {
+            total_distance_covered = total_distance_covered + distance_to_cover;
+            current_fuel = current_fuel - distance_to_cover;
+            // cout << "total distance covered: " << total_distance_covered  << "\n";
+            start = stops[i];
+            i++;
+            if(i == total_stop){
+                end = dist;
+            }
+            else {
+                end = stops[i];
+            }
+        }
+        // cout << "total_distace: " << total_distance_covered  << "\n";
+        // cout << "dist: " << dist  << "\n";
+    }
     return no_of_refill;
 }
 
